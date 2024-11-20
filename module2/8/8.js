d30f72ab32dca5c32359ff83fff0cb76f3480cb6
Daniel <100704382+Ristler@users.noreply.github.com>
2024-11-20 13:38:18 +0200

    Update 8.js
    fixed, forgot not to use join...

--- module2/8/8.js
+++ module2/8/8.js
@@ -3,6 +3,11 @@
 const names = ["Johnny", "DeeDee", "Joey", "Marky"];
 
 function concat(array) {
-    document.querySelector("#target").innerHTML = names.map(name => `${name}`).join('')
+    let concat = '';
+    for (i = 0; i < names.length; i++) {
+        concat += names[i]
+
+    }
+    document.querySelector("#target").innerHTML = concat
 }
 concat(names)
